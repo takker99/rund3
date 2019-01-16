3c63a2733f224624ce3389264e1b35b72ba1fd2f
Toshiyuki Masui <masui@pitecan.com>
2019-01-16 23:07:31 +0900

    ...

--- index.js
+++ index.js
@@ -7,9 +7,26 @@ function loadScript (url) {
   })
 }
 
+function loadStyle (url) {
+  return new Promise((resolve) => {
+    const script = document.createElement('link')
+    script.ref = "stylesheet"
+    script.type = "text/css"
+    script.href = url
+    script.onload = resolve
+    document.body.appendChild(script)
+  })
+}
+
 async function loadAllScripts (urls) {
   for (let url of urls) {
     await loadScript(url)
+  }
+}
+
+async function loadAllStyles (urls) {
+  for (let url of urls) {
+    await loadStyle(url)
   }
 }
 
@@ -20,11 +37,16 @@ $(function(){
     args[name] = decodeURIComponent(value)
   })
   const codelist = args['code']
+  const csslist = args['css']
 
   console.log('codelist', codelist)
 
   if (codelist) {
     let urls = codelist.split(/,/)
     loadAllScripts(urls)
+  }
+  if (csslist) {
+    let urls = csslist.split(/,/)
+    loadAllStyles(urls)
   }
 })
