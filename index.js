0f86e4d0b40752b0e6d4842831d3f123b9f29f04
Toshiyuki Masui <masui@pitecan.com>
2019-01-18 09:16:53 +0900

    短縮表記を許す

--- index.js
+++ index.js
@@ -1,52 +1,78 @@
+var project = "prog-exercises"
+
 function loadScript (url) {
-  return new Promise((resolve) => {
-    const script = document.createElement('script')
-    script.src = url
-    script.onload = resolve
-    document.body.appendChild(script)
-  })
+    return new Promise((resolve) => {
+	const script = document.createElement('script')
+	script.src = url
+	script.onload = resolve
+	document.body.appendChild(script)
+    })
 }
 
 function loadStyle (url) {
-  return new Promise((resolve) => {
-    const script = document.createElement('link')
-    script.rel = "stylesheet"
-    script.type = "text/css"
-    script.href = url
-    script.onload = resolve
-    document.body.appendChild(script)
-  })
+    return new Promise((resolve) => {
+	const script = document.createElement('link')
+	script.rel = "stylesheet"
+	script.type = "text/css"
+	script.href = url
+	script.onload = resolve
+	document.body.appendChild(script)
+    })
 }
 
 async function loadAllScripts (urls) {
-  for (let url of urls) {
-    await loadScript(url)
-  }
+    for (let url of urls) {
+	await loadScript(url)
+    }
 }
 
 async function loadAllStyles (urls) {
-  for (let url of urls) {
-    await loadStyle(url)
-  }
+    for (let url of urls) {
+	await loadStyle(url)
+    }
+}
+
+async function loadAllScripts2 (codes) {
+    for (let code of urls) {
+	await loadScript(`https://Scrapbox.io/${project}/${code}`)
+    }
+}
+
+async function loadAllStyles2 (styles) {
+    for (let style of styles) {
+	await loadStyle(`https://Scrapbox.io/${project}/${style}`)
+    }
 }
 
 $(function(){
-  const args = {}
-  document.location.search.substring(1).split('&').forEach((s) => {
-    let [name, value] = s.split('=')
-    args[name] = decodeURIComponent(value)
-  })
-  const codelist = args['code']
-  const csslist = args['css']
-
-  console.log('codelist', codelist)
-
-  if (codelist) {
-    let urls = codelist.split(/,/)
-    loadAllScripts(urls)
-  }
-  if (csslist) {
-    let urls = csslist.split(/,/)
-    loadAllStyles(urls)
-  }
+    const args = {}
+    document.location.search.substring(1).split('&').forEach((s) => {
+	let [name, value] = s.split('=')
+	args[name] = decodeURIComponent(value)
+    })
+    const codelist = args['code']
+    const csslist = args['css']
+    const p = args['p']
+    const clist = args['c']
+    const slist = args['s']
+    
+    if (codelist) {
+	let urls = codelist.split(/,/)
+	loadAllScripts(urls)
+    }
+    if (csslist) {
+	let urls = csslist.split(/,/)
+	loadAllStyles(urls)
+    }
+    if (p) {
+	project = p
+    }
+    if (p && clist) {
+	let codes = clist.split(/,/)
+	loadAllScripts2(codes)
+    }
+    if (p && slist) {
+	let styles = slist.split(/,/)
+	loadAllStyles22(styles)
+    }
 })
